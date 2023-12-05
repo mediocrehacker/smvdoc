b652680a2185a5ab5d15702775b668a6ae2ec6e9
mediocrehacker <mediocrehacker@prontonmail.com>
2023-12-05 20:02:40 +0100

    Add Open Graph tags

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,6 +6,26 @@ export const meta: MetaFunction = () => {
   return [
     { title: "Соснина Мария Викторовна" },
     { name: "description", content: "Соснина Мария Викторовна, врач-психотерапевт, клинический психолог, практика с 2004г." },
+    {
+      property: "og:title",
+      content: "Соснина Мария Викторовна, врач-психотерапевт, психолог",
+    },
+    {
+      property: "og:url",
+      content: "https://smvdoc.ru",
+    },
+    {
+      property: "og:image",
+      content: "https://smvdoc.ru/maria.jpg",
+    },
+    {
+      property: "og:type",
+      content: "article",
+    },
+    {
+      property: "og:description",
+      content: "Психологическое консультирование, практика с 2004г. Для записи на первичную консультацию писать в Телеграмм или Wats app +79050890284.",
+    },
   ];
 };
 
@@ -17,7 +37,8 @@ export default function Index() {
         <div className="flex flex-col gap-4">
           <h1 className="text-4xl">Соснина Мария Викторовна</h1>
           <h2 className="text-lg">врач-психотерапевт, клинический психолог</h2>
-          <h1 className="text-2xl">Практика с 2004г.</h1>
+          <h1 className="text-lg font-bold">ПСИХОЛОГИЧЕСКОЕ КОНСУЛЬТИРОВАНИЕ</h1>
+          <h2 className="text-lg">практика с 2004г.</h2>
           <p>Для записи на первичную консультацию писать в Телеграмм или Wats app <strong>+79050890284</strong>.</p>
           <p>г. Томск, пр. Ленина 30, стр 2, 2 подъезд, 2 этаж, офис 19.</p>
         </div>
